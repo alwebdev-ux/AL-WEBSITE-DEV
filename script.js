309e8fd300b6f9c4c384d8e586a202d98ab7f4aa
| AL-WEB-DEV [ <101592967+lewarrior@users.noreply.github.com>
2024-05-09 02:10:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuAMD = document.querySelector(".nav_bar")
+const navlink = document.querySelector(".but")
+
+menuAMD.addEventListener('click', ()=>{
+    navlink.classList.toggle('MLM')
+})
